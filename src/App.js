f7e425cacbe4cd2faaa51d832480daf4f2083b56
Piyush Verma <piyush98verma@gmail.com>
2021-08-28 16:52:34 +0530

    todo list project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import Todo from "./components/todo";
+import "./components/style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="main-heading">Todo list</h1>
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
